cb80a65efe13d8f6171906c1972e7303e6e7e2e4
Alvin <alvin@js-adways.com.tw>
2019-06-26 15:01:49 +0800

    upload fileupload.js

--- src/asset/fileupload/fileupload.js
+++ src/asset/fileupload/fileupload.js
@@ -59,3 +59,15 @@ $(document).on("click",".upload_remove",function (e) {
     e.preventDefault();
     $(this).parents('.upload_temp').remove();
 });
+
+function _uuid() {
+    var d = Date.now();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
